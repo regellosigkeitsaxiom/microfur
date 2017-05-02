b4eb1ababebbed3b5333e083ec0bb0e8254d0118
Valentin Shirokov <regellosigkeitsaxiom@yandex.ru>
2017-05-03 03:17:55 +0800

    Added templates and ability to specify files

--- default.nix
+++ default.nix
@@ -7,15 +7,16 @@ stdenv.mkDerivation rec {
   src = ./.;
   version = "1.0.0.0";
   name = "microfur-${version}";
-  buildInputs = with pkgs; [ stlink gcc-arm-embedded bash ];
+  buildInputs = with pkgs; [ openssh stlink gcc-arm-embedded bash ];
   buildPhase = ''
   '';
   installPhase = ''
     mkdir -p $out/bin
+    mkdir -p $out/etc
     cp microfur $out/bin/microfur
     chmod +x $out/bin/*
-    cp -r templates $out/templates
-    cp -r Cube $out/Cube
+    cp -r templates $out/etc/templates
+    cp -r Drivers $out/etc/Drivers
   '';
 }
 
